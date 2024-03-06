a6343da871ecafd4b97ed9177ec0a7c4dac3452c
Isaac Lee <lee3650@purdue.edu>
2024-03-06 14:34:46 -0500

    Added left click menu to template list

--- src/components/TemplatePanel/TemplateButton.tsx
+++ src/components/TemplatePanel/TemplateButton.tsx
@@ -1,19 +1,46 @@
 import React from 'react'
 import { FC } from 'react'
-import css from './TemplatePanel.module.css'
+import css, { spaceRight } from './TemplatePanel.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashCan, faEdit, faCopy, faCalendar } from '@fortawesome/free-regular-svg-icons'
 
-import { faTrashCan } from '@fortawesome/free-regular-svg-icons'
-
-export class TemplateLabel {
-    constructor(public label : string) {
+export class TemplateButtonProps {
+    constructor(public label : string, public index : number, public clicked : (index : number) => void, public selectedIndex : number) {
 
     }
 }
 
-export const TemplateButton : FC<TemplateLabel> = (prop : TemplateLabel) => {
-    return (<div className={css.button}>
+export const TemplateButton : FC<TemplateButtonProps> = (prop : TemplateButtonProps) => {    
+    const onclick = (e : any) => {
+        e.stopPropagation(); 
+        prop.clicked(prop.index); 
+    }    
+
+    const menuClicked = (e : any) => {
+        e.stopPropagation(); 
+    }
+
+    return (<div className={`${css.button} ${prop.selectedIndex == prop.index ? css.buttonActive : ''}`} onClick={onclick}>
         # {prop.label}
-        <FontAwesomeIcon icon={faTrashCan} className={css.deleteTemplateBtn}></FontAwesomeIcon>
+
+        <div className={`${css.leftClickMenu} ${prop.selectedIndex == prop.index ? css.menuActive : ''}`} onClick={menuClicked}>
+            <p>
+            <FontAwesomeIcon icon={faCalendar} className={spaceRight}></FontAwesomeIcon>
+            Load into today
+            </p>
+            <p>
+            <FontAwesomeIcon icon={faCopy} className={spaceRight}></FontAwesomeIcon>
+            Duplicate
+            </p>
+            <p>
+            <FontAwesomeIcon icon={faEdit} className={spaceRight}></FontAwesomeIcon>
+            Edit
+            </p>
+            <div/>
+            <p className={css.deleteTemplateBtn}>
+            <FontAwesomeIcon icon={faTrashCan} className={spaceRight}></FontAwesomeIcon>
+            Delete
+            </p>
+        </div>
     </div>)
 }
--- src/components/TemplatePanel/TemplatePanel.tsx
+++ src/components/TemplatePanel/TemplatePanel.tsx
@@ -1,20 +1,38 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useState } from 'react'
 import css from './TemplatePanel.module.css'
 import { faCircleLeft } from '@fortawesome/free-regular-svg-icons'
-import { TemplateButton, TemplateLabel } from './TemplateButton'
+import { TemplateButton, TemplateButtonProps } from './TemplateButton'
 import { NewTemplateButton } from './NewTemplateButton'
 
 export const TemplatePanel = () => {
+    const [selectedIndex, setSelectedIndex] = useState(-1); 
+
+    const onClicked = (index : number) => {
+        if (index == selectedIndex)
+        {
+            setSelectedIndex(-1); 
+        }
+        else 
+        {
+            setSelectedIndex(index); 
+        }
+    }
+
+    const clickOff = () => {
+        setSelectedIndex(-1); 
+    }
+
     return (
-        <div className={css.container}>
+        <div className={css.container} onClick={clickOff}>
             <FontAwesomeIcon icon={faCircleLeft} className={css.back}></FontAwesomeIcon>
             <p className={css.login}>login</p>
             <h1>templates</h1>
             <div className={css.buttonContainer}>
-                <TemplateButton {...new TemplateLabel('blank')}/>
-                <TemplateButton {...new TemplateLabel('example')}/>
-                <TemplateButton {...new TemplateLabel('abcdefg')}/>
-                <TemplateButton {...new TemplateLabel('example')}/>
+                <TemplateButton {...new TemplateButtonProps('blank', 0, onClicked, selectedIndex)}/>
+                <TemplateButton {...new TemplateButtonProps('example', 1, onClicked, selectedIndex)}/>
+                <TemplateButton {...new TemplateButtonProps('abcdefg', 2, onClicked, selectedIndex)}/>
+                <TemplateButton {...new TemplateButtonProps('example', 3, onClicked, selectedIndex)}/>
                 <NewTemplateButton/>
             </div>
         </div>
