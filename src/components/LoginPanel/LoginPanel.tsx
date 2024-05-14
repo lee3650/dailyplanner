deaeb28e11ac52d1c1552c05ae0eb82d670b8177
Isaac Lee <lee3650@purdue.edu>
2024-05-14 18:05:49 -0500

    Fixed login panel visuals

--- src/components/LoginPanel/LoginPanel.tsx
+++ src/components/LoginPanel/LoginPanel.tsx
@@ -4,13 +4,19 @@ export const LoginPanel = () => {
     return (
         <div className={css.container}>
             <div className={css.panel}>
-                <h3>daily planner</h3>
-                <input type="text" placeholder='email'/>
-                <input type="text" placeholder='password'/>
-                <button>Sign In</button>
-                <p>Forgot password?</p>
-                <p>Sign in with</p>
-                <p>Or continue as guest</p>
+                <div className={css.logo}>
+                <b>禅</b>
+                </div>
+                <h1>daily planner</h1>
+                <input type="email" placeholder='email'/>
+                <br/>
+                <input type="password" placeholder='password'/>
+                <br/>
+                <button><b>Sign In</b></button>
+                <p><b>Forgot password?</b></p>
+                <label>Sign in with</label>
+                <img src='https://banner2.cleanpng.com/20180324/sww/kisspng-google-logo-g-suite-chrome-5ab6e618b3b2c3.5810634915219358967361.jpg' alt='google logo'></img>
+                <p><b>Or continue as guest</b></p>
             </div>
         </div>
     )
