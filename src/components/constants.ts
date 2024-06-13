64eae1348179d84d9155638592172c644d901dcb
Isaac Lee <lee3650@purdue.edu>
2024-06-13 09:46:42 -0500

    Added s to base url

--- src/components/constants.ts
+++ src/components/constants.ts
@@ -1,7 +1,7 @@
 export const GUEST_ID = -1; 
 export const UNINIT_ID = -100; 
 
-export const BASE_URL = 'http://localhost:8080/'
+export const BASE_URL = 'https://localhost:8080/'
 export const LOGIN_URL = BASE_URL + 'login'
 export const CREATE_ACCOUNT_URL = BASE_URL + 'createAccount'
 export const readTemplateUrl = (id : number) => BASE_URL + `readTemplate/${id}`
